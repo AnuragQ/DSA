dad5c471e42048947ce900c2d86069a6edacbf5d
anurag <anurags153@gmail.com>
2020-06-05 22:17:24 +0530

    new questions in summary

---
+++ java/2dArrays/SubarrayTargetSum.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+class SubArrayTargetSum {
+    public static void main(String[] args) throws IOException {
+        // code
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String inp = br.readLine();
+            String[] inpArr=inp.split(" ");
+            int n = Integer.parseInt(inpArr[0]);
+            int target = Integer.parseInt(inpArr[1]);
+            String nums = br.readLine();
+            long[] arr = new long[n];
+            String[] numsarr=nums.split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(numsarr[i]);
+            }
+            long sum = arr[0];
+            int left = 0, right = 1;
+            while ((left <= right && sum != target)) {
+                if (right < n && sum < target) {
+                    sum += arr[right];
+                    right++;
+                } else if (left < n) {
+                    sum -= arr[left];
+                    left++;
+                }
+                // System.out.println(sum);
+            }
+            if (sum == target) {
+                System.out.println((left + 1) + " " + (right));
+
+            } else {
+                System.out.println("-1");
+            }
+
+        }
+
+        br.close();
+    }
+
+}
