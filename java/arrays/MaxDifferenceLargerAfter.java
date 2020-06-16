ff492172906867014577ceb56ea3f30191875280
anurag <anurags153@gmail.com>
2020-06-16 13:46:39 +0530

    array questions

---
+++ java/arrays/MaxDifferenceLargerAfter.java
@@ -0,0 +1,41 @@
+package arrays;
+
+/*package whatever //do not write package name here */
+
+// import java.util.*;
+// import java.lang.*;
+import java.io.*;
+
+class MaxDifferenceLargerAfter {
+	public static void main(String[] args) throws IOException {
+		// code
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt((br.readLine()));
+			String[] inp = br.readLine().split(" ");
+			int[] arr = new int[inp.length];
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(inp[i]);
+			}
+
+            int max=Integer.MIN_VALUE;
+            int ans=Integer.MIN_VALUE;
+            for(int i=n-1;i>=0;i--){
+                if(arr[i]<max){
+                    ans=Math.max(ans,max-arr[i]);
+                }else{
+                    max=arr[i];
+                }
+            }
+            
+            System.out.println(Math.max(ans,-1));
+        
+		}
+		
+
+		br.close();
+
+	}
+}
---
+++ java/arrays/MinSwapsKTogether.java
@@ -0,0 +1,55 @@
+package arrays;
+
+/*package whatever //do not write package name here */
+
+// import java.util.*;
+// import java.lang.*;
+import java.io.*;
+
+class MinSwapsKTogether {
+	public static void main (String[] args) throws IOException{
+		//code
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt((br.readLine()));
+			String[] inp = br.readLine().split(" ");
+			int[] arr = new int[inp.length];
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(inp[i]);
+			}
+			int k = Integer.parseInt((br.readLine()));
+// 			System.out.println(k);
+			int good=0;
+			for (int i=0;i<n;i++){
+			   if(arr[i]<=k){
+			        good++;
+			    } 
+			}
+			int ans=0,bad=0;
+			for(int i=0; i<good;i++){
+			    if(arr[i]>k){
+			        bad++;
+			    }
+			}
+			ans=bad;
+			for(int i=0,j=good;j<arr.length;i++,j++){
+			    if(arr[i]>k){
+			        bad--;
+			    }
+			    if(arr[j]>k){
+			        bad++;
+			    }
+			    ans=Math.min(ans,bad);
+			}
+			System.out.println(ans);
+			
+
+		}
+
+		br.close();
+		
+	}
+}
