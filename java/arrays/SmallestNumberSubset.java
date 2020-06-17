5afc1fc8de73e3015aded763a984ada69c98b35e
anurag <anurags153@gmail.com>
2020-06-17 07:21:39 +0530

    array questions

---
+++ java/arrays/SmallestNumberSubset.java
@@ -0,0 +1,44 @@
+package arrays;
+
+/*package whatever //do not write package name here */
+
+// import java.util.*;
+// import java.lang.*;
+import java.io.*;
+
+class SmallestNumberSubset {
+	public static void main(String[] args) throws IOException {
+		// code
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+			int n = Integer.parseInt((br.readLine()));
+			String[] inp = br.readLine().split(" ");
+			int[] arr = new int[inp.length];
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(inp[i]);
+			}
+            int max=1;
+            boolean found=false;
+            for (int i=0;i<n;i++){
+                if(arr[i]>max){
+                    System.out.println(max);
+                    found=true;
+                    break;
+                }else{
+                    max+=arr[i];
+                }
+            }
+            if(!found){
+                System.out.println(max);
+            }
+
+
+
+		}
+
+		br.close();
+
+	}
+}
