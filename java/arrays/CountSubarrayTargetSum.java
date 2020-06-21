3992c02dc7228f4c2dd9e5bff263f9fdf46f339d
anurag <anurags153@gmail.com>
2020-06-21 08:55:28 +0530

    array questions

--- java/arrays/CountSubarrayTargetSum.java
+++ java/arrays/CountSubarrayTargetSum.java
@@ -49,3 +49,18 @@ public class CountSubarrayTargetSum {
 
     }
 }
+// LEETCODE
+// https://leetcode.com/problems/subarray-sum-equals-k/submissions/
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         HashMap<Integer,Integer> hm=new HashMap<>();
+//         int sum=0, count=0;
+//         hm.put(0,1);
+//         for(int i=0;i<nums.length;i++){
+//             sum+=nums[i];
+//             count+=hm.getOrDefault(sum-k,0);
+//             hm.put(sum,hm.getOrDefault(sum,0)+1);           
+//         }
+//         return count;         
+//     }
+// }
